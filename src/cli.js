b61b2206f9422ea2f47b8d7695d2ed2cc43b55ff
John Smith <rovaro@list.ru>
2017-07-04 20:23:03 +0300

    Create cli.js

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import program from 'commander';
+
+export default () => {
+  program
+    .version('0.1.0')
+    .arguments('<firstConfig> <secondConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .parse(process.argv);
+};
