fdc45b44b4cc1ac33c2e69f9e1fed171c6433864
John Smith <rovaro@list.ru>
2017-07-04 09:15:46 +0300

    Update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import data from '../data';
 
-program
-  .version('0.1.0')
-  .arguments('<firstConfig> <secondConfig>')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
-.parse(process.argv);
+data();
+
