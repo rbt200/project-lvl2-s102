3c3acffb5f9c7782a456715caa8d37cfd4cb745d
John Smith <rovaro@list.ru>
2017-07-04 21:39:26 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,11 @@
 import program from 'commander';
-import { bin } from 'package';
+import {version} from '../package';
 
 export default () => {
   program
-    .version(bin)
+    .version(version)
     .arguments('<firstConfig> <secondConfig>')
-    .description(bin)
+    .description(version)
     .option('-f, --format [type]', 'Output format')
     .parse(process.argv);
 };
