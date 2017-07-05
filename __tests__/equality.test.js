266d538d9a86e7721a941c7653e6dec9b153c617
John Smith <rovaro@list.ru>
2017-07-05 18:08:08 +0300

    Update equality.test.js

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -2,7 +2,7 @@ import gendiff from '../src/bin/gendiff.js';
 
 const before = 'before.json';
 const after = 'after.json';
-const result = 'result';
+const result = 'result.json';
 
 test('Compare two .json files', () => {
   expect(gendiff(before, after)).toBe(result);
