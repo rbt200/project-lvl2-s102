985ee63126f68bd6b802220dcce01dcfa53056b0
John Smith <rovaro@list.ru>
2017-07-05 18:24:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import fs from 'fs';
 import _ from 'lodash';
-console.log(fs.readFileSync(path));
+
+export default (first, second) => 
+const res = {
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+}
+return res;
+};
