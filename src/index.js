0872100641522b77ecd7555ea3dfb25941819003
John Smith <rovaro@list.ru>
2017-07-06 20:31:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import _ from 'lodash';
+import path from 'path';
+import yml from 'js-yaml';
+//var doc = yaml.safeLoad(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
+const getExtention = (filePath) => {
+  const ext = path.parse(filePath).ext;
+  console.log(ext);
+};
+
 
 export default (first, second) => {
   const f = JSON.parse(fs.readFileSync(first).toString());
   const s = JSON.parse(fs.readFileSync(second).toString());
-
+getExtention(first);
   let res = '';
   _.forEach(f, (value, key) => {
     _.forEach(s, (value2, key2) => {
