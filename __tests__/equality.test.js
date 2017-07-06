7851ca8f942bea5302a53800566024fbf68b684a
John Smith <rovaro@list.ru>
2017-07-06 19:38:16 +0300

    add yml test

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -1,9 +1,17 @@
 import gendiff from '../src';
 
-const before = '__tests__/fixtures/json/before.json';
-const after = '__tests__/fixtures/json/after.json';
-const result = '{\n    host: \"hexlet.io\"\n  + timeout: 20\n  - timeout: 50\n  - proxy: "123.234.53.22"\n  + verbose: "true"\n}';
-
 test('Compare two .json files', () => {
+  const before = '__tests__/fixtures/json/before.json';
+  const after = '__tests__/fixtures/json/after.json';
+  const result = '{\n    host: \"hexlet.io\"\n  + timeout: 20\n  - timeout: 50\n  - proxy: "123.234.53.22"\n  + verbose: "true"\n}';
+
+  expect(gendiff(before, after)).toBe(result);
+});
+
+test('Compare two .yml files', () => {
+  const before = '__tests__/fixtures/yml/before.yml';
+  const after = '__tests__/fixtures/yml/after.yml';
+  const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+
   expect(gendiff(before, after)).toBe(result);
 });
