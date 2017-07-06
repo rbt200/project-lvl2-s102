a60fcd77f02bd2182514be9e3863a23697ec596a
John Smith <rovaro@list.ru>
2017-07-06 19:44:48 +0300

    change test to it

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -1,17 +1,17 @@
 import gendiff from '../src';
 
-test('Compare two .json files', () => {
+describe('gendiff tests', () => {  
+  it('Compare two .json files', () => {
   const before = '__tests__/fixtures/json/before.json';
   const after = '__tests__/fixtures/json/after.json';
   const result = '{\n    host: \"hexlet.io\"\n  + timeout: 20\n  - timeout: 50\n  - proxy: "123.234.53.22"\n  + verbose: "true"\n}';
-
   expect(gendiff(before, after)).toBe(result);
-});
-
-test('Compare two .yml files', () => {
+  });
+  
+  it('Compare two .yml files', () => {
   const before = '__tests__/fixtures/yml/before.yml';
   const after = '__tests__/fixtures/yml/after.yml';
   const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
-
   expect(gendiff(before, after)).toBe(result);
+  });
 });
