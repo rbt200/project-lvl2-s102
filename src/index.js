fd0d2c81bbf93aff383343f37a476380af1073fd
myname <myesoap@mail.ru>
2017-07-06 12:54:46 +0300

    change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import fs from 'fs';
 import _ from 'lodash';
 
 export default (first, second) => {
-const res = {
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}
-return res;
+  const f = JSON.parse(fs.readFileSync(first).toString());
+  const s = JSON.parse(fs.readFileSync(second).toString());
+
+  let res = '';
+  _.forEach(f, (value, key) => {
+    _.forEach(s, (value2, key2) => {
+      if (key === key2 && value === value2) { res += `    ${key}: "${value}"\n`; }
+      if (key === key2 && value !== value2) { res += `  + ${key2}: ${value2}\n`; res += `  - ${key}: ${value}\n`; }
+    });
+  });
+
+  _.forEach(f, (value, key) => {
+    if (!_.has(s, key)) { res += `  - ${key}: "${value}"\n`; }
+  });
+
+  _.forEach(s, (value, key) => {
+    if (_.isEmpty(_.pick(f, key))) { res += `  + ${key}: "${value}"\n`; }
+  });
+
+  return `{\n${res}}`;
 };
