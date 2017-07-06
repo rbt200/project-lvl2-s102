bb0a0c28bf0c792fe2a0b580e441d730843eb731
myname <myesoap@mail.ru>
2017-07-06 22:00:07 +0300

    task #2 ready

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -1,17 +1,17 @@
 import gendiff from '../src';
 
-describe('gendiff tests', () => {  
+describe('gendiff tests', () => {
+  const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+
   it('Compare two .json files', () => {
   const before = '__tests__/fixtures/json/before.json';
-  const after = '__tests__/fixtures/json/after.json';
-  const result = '{\n    host: \"hexlet.io\"\n  + timeout: 20\n  - timeout: 50\n  - proxy: "123.234.53.22"\n  + verbose: "true"\n}';
+  const after = '__tests__/fixtures/json/after.json'; 
   expect(gendiff(before, after)).toBe(result);
   });
   
   it('Compare two .yml files', () => {
   const before = '__tests__/fixtures/yml/before.yml';
   const after = '__tests__/fixtures/yml/after.yml';
-  const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
   expect(gendiff(before, after)).toBe(result);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,31 +2,43 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 import yml from 'js-yaml';
-//var doc = yaml.safeLoad(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
-const getExtention = (filePath) => {
-  const ext = path.parse(filePath).ext;
-  console.log(ext);
+
+const getExtention = filePath => path.parse(filePath).ext;
+
+const parseFile = (file) => {
+  const ext = getExtention(file);
+  let result;
+  switch (ext) {
+    case '.json' :
+      result = JSON.parse(fs.readFileSync(file).toString());
+      break;
+    case '.yml' || 'yaml':
+      result = yml.safeLoad(fs.readFileSync(file, 'utf8').toString());
+      break;
+    default:
+      break;
+  }
+  return result;
 };
 
-
 export default (first, second) => {
-  const f = JSON.parse(fs.readFileSync(first).toString());
-  const s = JSON.parse(fs.readFileSync(second).toString());
-getExtention(first);
+  const f = parseFile(first);
+  const s = parseFile(second);
+  getExtention(first);
   let res = '';
   _.forEach(f, (value, key) => {
     _.forEach(s, (value2, key2) => {
-      if (key === key2 && value === value2) { res += `    ${key}: "${value}"\n`; }
+      if (key === key2 && value === value2) { res += `    ${key}: ${value}\n`; }
       if (key === key2 && value !== value2) { res += `  + ${key2}: ${value2}\n`; res += `  - ${key}: ${value}\n`; }
     });
   });
 
   _.forEach(f, (value, key) => {
-    if (!_.has(s, key)) { res += `  - ${key}: "${value}"\n`; }
+    if (!_.has(s, key)) { res += `  - ${key}: ${value}\n`; }
   });
 
   _.forEach(s, (value, key) => {
-    if (_.isEmpty(_.pick(f, key))) { res += `  + ${key}: "${value}"\n`; }
+    if (_.isEmpty(_.pick(f, key))) { res += `  + ${key}: ${value}\n`; }
   });
 
   return `{\n${res}}`;
