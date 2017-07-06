250cf775d3199b757cfab60da0c3e8b1497d315f
John Smith <rovaro@list.ru>
2017-07-06 18:45:12 +0300

    Update equality.test.js

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -1,7 +1,7 @@
 import gendiff from '../src';
 
-const before = '__tests__/fixtures/before.json';
-const after = '__tests__/fixtures/after.json';
+const before = '__tests__/fixtures/json/before.json';
+const after = '__tests__/fixtures/json/after.json';
 const result = '{\n    host: \"hexlet.io\"\n  + timeout: 20\n  - timeout: 50\n  - proxy: "123.234.53.22"\n  + verbose: "true"\n}';
 
 test('Compare two .json files', () => {
