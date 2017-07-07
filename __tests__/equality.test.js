566076174d44f5bb4a8299fece61b63f350c8fe5
myname <myesoap@mail.ru>
2017-07-07 15:20:34 +0300

    change

--- __tests__/equality.test.js
+++ __tests__/equality.test.js
@@ -4,14 +4,14 @@ describe('gendiff tests', () => {
   const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
 
   it('Compare two .json files', () => {
-  const before = '__tests__/fixtures/json/before.json';
-  const after = '__tests__/fixtures/json/after.json'; 
-  expect(gendiff(before, after)).toBe(result);
+    const before = '__tests__/fixtures/json/before.json';
+    const after = '__tests__/fixtures/json/after.json';
+    expect(gendiff(before, after)).toBe(result);
   });
 
   it('Compare two .yml files', () => {
-  const before = '__tests__/fixtures/yml/before.yml';
-  const after = '__tests__/fixtures/yml/after.yml';
-  expect(gendiff(before, after)).toBe(result);
-  });  
+    const before = '__tests__/fixtures/yml/before.yml';
+    const after = '__tests__/fixtures/yml/after.yml';
+    expect(gendiff(before, after)).toBe(result);
+  });
 });
